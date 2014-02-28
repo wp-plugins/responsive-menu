ee32ec5042c8f61b98d3b9ba206da80a4f292280
peter.featherstone <peter.featherstone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-28 12:37:23 +0000

    Updated responsiveMenu class
    git-svn-id: https://plugins.svn.wordpress.org/responsive-menu/tags/1.2@866917 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/class.responsiveMenu.php
+++ classes/class.responsiveMenu.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+    Main Class for Responsive Menu
+ 
+    Copyright 2014  Peter Featherstone <peter.featherstone@networkintellect.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 namespace PeterFeatherstone\ResponsiveMenu;
 
 class ResponsiveMenu {
@@ -21,6 +40,7 @@ class ResponsiveMenu {
                     'responsiveMenuBackgroundColour' => '#000000',
                     'responsiveMenuButtonTitle' => '',
                     'responsiveMenuBackgroundTransparent' => 'checked'
+                    
             ) ) );    
         
     }
@@ -609,7 +629,7 @@ $css .= "   }
     
     private function checkViewPortTag() {
 
-        $metaTags = get_meta_tags( get_bloginfo( 'url' )  );
+        $metaTags = get_meta_tags( get_bloginfo( 'url' ) );
         
         if( $metaTags['viewport'] ) return $metaTags['viewport'];
                           
