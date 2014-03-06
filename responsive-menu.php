1077574665c96f6ea36157bd916d90a5ddcc60da
peter.featherstone <peter.featherstone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-06 11:06:33 +0000

    1.6 Released
    git-svn-id: https://plugins.svn.wordpress.org/responsive-menu/tags/1.6@870514 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- responsive-menu.php
+++ responsive-menu.php
@@ -4,7 +4,7 @@
 Plugin Name: Responsive Menu
 Plugin URI: http://www.peterfeatherstone.com/responsive-menu/
 Description: Highly Customisable Responsive Menu Plugin Created By Peter Featherstone @ Network Intellect.
-Version: 1.5
+Version: 1.6
 Author: Peter Featherstone
 Author URI: http://www.peterfeatherstone.com/responsive-menu/
 License: GPL2
