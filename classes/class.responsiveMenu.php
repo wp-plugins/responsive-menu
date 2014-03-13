72a8f67ff5ed045e0a64a16513852a2ff85b3134
peter.featherstone <peter.featherstone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-13 17:41:57 +0000

    Updated email address
    git-svn-id: https://plugins.svn.wordpress.org/responsive-menu/tags/1.7@874894 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/class.responsiveMenu.php
+++ classes/class.responsiveMenu.php
@@ -2,7 +2,7 @@
 /*
   Main Class for Responsive Menu
 
-  Copyright 2014  Peter Featherstone <peter.featherstone@networkintellect.com>
+  Copyright 2014  Peter Featherstone <hello@peterfeatherstone.com>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2, as
