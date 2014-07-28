f9da782c9a89e6934c52d93d1a6b9c5d1fb1ed9d
peter.featherstone <peter.featherstone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-28 18:21:09 +0000

    Updated 1.10
    git-svn-id: https://plugins.svn.wordpress.org/responsive-menu/tags/1.10@956158 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ app/config.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = array( 
+    
+    
+    'current_version' => 1.10,
+    
+    
+    'plugins_dir' => plugin_dir_path( __FILE__ ),
+        
+    
+    'plugins_base_uri' => plugin_dir_url( __FILE__ ),
+    
+    
+    'plugin_base_dir' => dirname( plugin_dir_path( __FILE__ ) ),
+    
+    
+    'plugin_data_uri' => plugin_dir_url( dirname( __FILE__ ) ) . 'responsive-menu-data/',
+    
+    
+    'plugin_data_dir' => dirname( plugin_dir_path( __FILE__ ) ) . '/responsive-menu-data/',
+    
+    
+);
