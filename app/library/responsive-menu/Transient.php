8ecda17e2133c5644d067fd7109c8f632ee1939c
peter.featherstone <peter.featherstone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-21 10:25:35 +0000

    Removed beta staging to beta tag
    git-svn-id: https://plugins.svn.wordpress.org/responsive-menu/tags/beta@1164860 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ app/library/responsive-menu/Transient.php
@@ -0,0 +1,89 @@
+<?php
+
+class RM_Transient {
+    
+    /**
+     * Function to get named cached transient menu
+     *
+     * @param  string  $name
+     * @return string
+     * @added 2.3
+     * @edited 2.4 - Added option to use transient caching
+     */
+    
+    static function getTransientMenu( $name ) {
+
+        $Transient = ResponsiveMenu::getOption( 'RMUseTran' );
+
+        if( $Transient ) :
+            
+            $cachedKey = $name . '_' . get_current_blog_id();
+            $cachedMenu = get_transient( $cachedKey );
+            
+        else :
+            
+            $cachedMenu = false;
+        
+        endif;
+
+        if( $cachedMenu === false ) :
+
+            $cachedMenu = self::createTransientMenu( $name );
+
+            if( $Transient )
+                set_transient( $cachedKey, $cachedMenu );
+        
+        endif;
+        
+        return $cachedMenu;
+        
+    }
+    
+     /**
+     * Function to create named cached transient menu
+     *
+     * @param  string  $name
+     * @return array
+     * @added 2.3
+     */
+    
+    static function createTransientMenu( $name ) {
+        
+        $walker = ResponsiveMenu::getOption( 'RMWalker' );
+        $depth = ResponsiveMenu::getOption( 'RMDepth' );
+        
+        $cachedMenu = wp_nav_menu( array(
+                'menu' => $name,
+                'menu_class' => 'responsive-menu',
+                'depth' => $depth,
+                'walker' => ( !empty( $walker ) ) ? new $walker : '', // Add by Mkdgs
+                'echo' => false 
+                )
+            );
+        
+        return $cachedMenu;
+        
+    }
+    
+    /**
+     * Function to clear all transient menus
+     *
+     * @return null
+     * @added 2.3
+     */
+    
+    static function clearTransientMenus() {
+        
+        if( ResponsiveMenu::hasMenus() ) :
+
+            foreach( ResponsiveMenu::getMenus() as $menu ) :
+
+                delete_transient( $menu->slug . '_' . get_current_blog_id() );
+
+            endforeach;
+
+        endif;
+        
+    }
+    
+}
